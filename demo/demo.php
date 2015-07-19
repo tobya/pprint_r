a12a7a488025074633ef3f4076b080d396d63c04
Toby Allen <toby@toflidium.com>
2015-07-19 13:31:44 +0100

    Updates to Demo files

--- demo/demo.php
+++ demo/demo.php
@@ -7,9 +7,12 @@
  $ShowArray =  array( 'Value1','Item2'=> 'Value2',
                       'Item3 (Click to Open)'=> array('Item4', 
                       'Item5' => 'Some Text value',
-                      'Item6' => "<a href='http://example.com>A Link</a> inside some HTML but it gets show just as is.'",
+                      'Item6' => "<a href='http://example.com'>A Link</a> inside some HTML but it gets show just as is.",
                       'Item7 (Click to Open)' => array('A Further Item', 'Another Item', 'Must Keep Going')));
 
   pprint_R($ShowArray);
   
+  echo '<P>Without pprint_r</P><PRE>';
+  print_R($ShowArray);
+
   echo '<P>The page continues after the call';
--- demo/demo_exit_string.php
+++ demo/demo_exit_string.php
@@ -10,9 +10,15 @@
  $ShowArray =  array( 'Value1','Item2'=> 'Value2',
                       'Item3 (Click to Open)'=> array('Item4', 
                       'Item5' => array('Some Text value'),
-                      'Item6' => "<a href='http://example.com>A Link</a> inside some HTML but it gets show just as is.'",
+                      'Item6' => "<a href='http://example.com'>A Link</a> inside some HTML but it gets show just as is.",
                       'Item7 (Click to Open)' => array('A Further Item', 'Another Item', 'Must Keep Going')));
 
+
+  echo '<P>Without pprint_r</P><PRE>';
+  print_R($ShowArray);
+  echo '</PRE>';
+
+  
   pprint_R($ShowArray,'pprint_r has shut down this page.');
 
   echo 'This statement wont be shown';
