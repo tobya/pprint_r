96c9b46ccd1ad9513177a01d30bf86cff0ede2c7
Toby Allen <toby@toflidium.com>
2015-09-02 21:43:47 +0100

    Revert "Added a placeholder to show an empty item."
    This reverts commit 43edb210af70cc2dd93f7f3bfef15f65326e3aa7.

--- pprint_r.php
+++ pprint_r.php
@@ -1,14 +1,14 @@
 <?php
 /*************************************************************
-Copyright © 2012 Toby Allen (http://github.com/tobya)
+Copyright Â© 2012 Toby Allen (http://github.com/tobya)
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, 
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the â€œSoftwareâ€), to deal in the Software without restriction, 
 including without limitation the rights to use, copy, modify, merge, publish, distribute, sub-license, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
 subject to the following conditions:
 
 The above copyright notice, and every other copyright notice found in this software, and all the attributions in every file, and this permission notice shall be included in all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. 
+THE SOFTWARE IS PROVIDED â€œAS ISâ€, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. 
 IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
 ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ****************************************************************/
@@ -122,8 +122,7 @@ function php_tree_array($MArray, $return_link, $extensions = array(), $first_cal
 		if( $first_call ) { $html_array_tree .= " class=\"php-array-tree\""; $first_call = false; }
 		$html_array_tree .= ">";
 		foreach( $MArray as $KeyValue => $this_value) {
-
-        if (trim($KeyValue) == ''){$KeyValue = '{EmptyKey}';}
+				 if (trim($KeyValue) == ''){$KeyValue = '{EmptyKey}';}
 				if( is_array($this_value) ) {
 					// Array
           $html_array_tree .= "\n<li class=\"pft-array\"><span href=\"#\">" . htmlspecialchars($KeyValue) . "</span>";
